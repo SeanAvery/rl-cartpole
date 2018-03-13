13c0e448ee1696b81d2c63312acd43139bc8f699
Sean Pollock <seanpollock@Seans-MacBook-Pro.local>
2018-03-12 23:36:20 -0400

    changing from util fns to class

--- neural_network/Model.py
+++ neural_network/Model.py
@@ -4,34 +4,41 @@ from keras.layers import Dense
 from keras.optimizers import Adam
 
 '''
-    MODEL UTILS
-    from Model import *
+    MODEL CLASS
 '''
 
-'''
-    returns keras model
-        12 --> 12 --> 2 Dense Network
-'''
-def build_model():
-    model = Sequential()
-    model.add(Dense(12, activation='relu', input_dim=4))
-    model.add(Dense(12, activation='relu'))
-    model.add(Dense(2))
-    model.compile(Adam(lr=0.001), 'mse')
-    return model
+class Model():
+    def __init__(self, model_name):
+        self.model_name = model_name
+        self.build_model()
 
-'''
-    params
-        model: [json] keras model
-        model_name: [string]
-    writes json to ./models/model_name.json
-'''
-def save_model(model, model_name):
-    if not os.path.exists('./models'):
-        os.makedirs('./models')
-    model_file = open('./models/{}.json'.format(model_name), 'w')
-    model_file.write(model)
-    model_file.close()
+    '''
+        returns keras model
+            12 --> 12 --> 2 Dense Network
+    '''
+    def build_model(self):
+        model = Sequential()
+        model.add(Dense(12, activation='relu', input_dim=4))
+        model.add(Dense(12, activation='relu'))
+        model.add(Dense(2))
+        model.compile(Adam(lr=0.001), 'mse')
+        self.model = model
+
+    '''
+        params
+            model: [json] keras model
+            model_name: [string]
+        writes json to ./models/model_name.json
+    '''
+    def save_model(self):
+        if not os.path.exists('./models'):
+            os.makedirs('./models')
+        model_file = open('./models/{}.json'.format(self.model_name), 'w')
+        model_file.write(model)
+        model_file.close()
+
+    def save_weights(self):
+
 
 ''' TESTING '''
 if __name__ == '__main__':
