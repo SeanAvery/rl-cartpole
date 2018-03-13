ce236a888be130372aaf67373f0f2b63608521b7
Sean Pollock <seanpollock3344@gmail.com>
2018-03-13 16:42:22 -0400

    adding in epsilon graph

--- neural_network/Graph.py
+++ neural_network/Graph.py
@@ -8,16 +8,25 @@ class Graph():
     def init_results_graph(self):
         self.fig, self.ax = plt.subplots()
         self.ax.set_ylim(0, 300)
-
         plt.draw()
         self.x, self.y = [], []
 
     def append_results_graph(self, episode, ticks):
-        print('append to graph', episode, ticks)
         self.x.append(episode)
         self.y.append(ticks)
         self.ax.plot(self.x, self.y)
 
+    def init_epsilon_graph(self):
+        self.fig2, self.ax2 = plt.subplots()
+        self.ax.set_ylim(0, 1)
+        plt.draw()
+        self.epsilon_x = []
+        self.epsilon_y = []
+
+    def append_epsilon_graph(self, episode, epsilon):
+        self.x_epsilon.append(episode)
+        self.y_epsilon.append(epsilon)
+        self.ax2.plot(self.x_epsilon, self.y_epsilon)
 
 ''' TESTING '''
 
