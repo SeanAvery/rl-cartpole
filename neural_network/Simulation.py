97e348b5d318d7f850e8719fb96522c6cd7ec299
Sean Pollock <seanpollock3344@gmail.com>
2018-03-13 16:36:39 -0400

    new replay fn is not fixing issue

--- neural_network/Simulation.py
+++ neural_network/Simulation.py
@@ -50,22 +50,21 @@ class Simulation():
 
     def replay(self):
         x_batch, y_batch = [], []
-        mini_batch = random.sample(self.memory, min(len, self.memory, self.Model.batch_size))
+        mini_batch = random.sample(self.memory, min(len(self.memory), self.Model.batch_size))
 
         for old_state, action, reward, new_state, done in mini_batch:
             y_target = self.Model.model.predict(old_state)
-            print('y_target', y_target)
             if done:
                 y_target[0][action] = reward
             else:
-                y_target[0][action] = rward + self.Model.gamma * np.max(self.Model.model.predict(new_state)[0])
-            x_batch.append(state[0])
+                y_target[0][action] = reward + self.Model.gamma * np.max(self.Model.model.predict(new_state)[0])
+            x_batch.append(old_state[0])
             y_batch.append(y_target[0])
 
-        self.model.fit(np.array(x_batch), np.array(y_batch), batch_size=len(x_batch), verbose=0)
+        self.Model.model.fit(np.array(x_batch), np.array(y_batch), batch_size=len(x_batch), verbose=0)
 
-        if self.Model.epsilon > self.Model.min_epsilon:
-            self.epsilon *= self.epsilon_decay
+        if self.Model.epsilon > self.Model.epsilon_min:
+            self.Model.epsilon *= self.Model.epsilon_decay
 
 ''' TESTING '''
 if __name__ == '__main__':
