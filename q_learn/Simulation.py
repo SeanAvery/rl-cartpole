531e5d4b68df5199f82b890417bd1e42e1a2783b
Sean Pollock <seanpollock3344@gmail.com>
2018-03-21 12:41:29 -0400

    adding epsilon and alpha decay fns ;

--- q_learn/Simulation.py
+++ q_learn/Simulation.py
@@ -14,15 +14,40 @@ class Simulation():
 
         self.q_table = np.zeros(self.buckets + (self.env.action_space.n,))
         print('empty q_table', self.q_table, self.q_table.shape)
+        self.epsilon = 1
         self.min_epsilon = 0.05
+        self.epsilon_decay = 0.97
+        self.alpha = 1
         self.min_alpha = 0.05
+        self.alpha_decay = 0.97
         self.gamma = 0.995
 
         print('lower bounds', self.lower_bounds)
         print('upper bounds', self.upper_bounds)
 
     def choose_action(self, state):
-        return 0 if state[3] < 5 else 1
+        if np.random.random() <= epsilon:
+            return self.env.action_space.sample()
+        else:
+            # choose from q-table
+
+    def calc_epsilon(self):
+        epsilon = self.epsilon * self.epsilon_decay
+        if epsilon < self.min_epsilon:
+            self.epsilon = self.min_epsilon
+        else:
+            self.epsilon = epsilon
+
+    def calc_alpha(self):
+        alpha = self.alpha * self.alpha_decay
+        if alpha < self.min_alpha:
+            self.alpha = self.min_alpha
+        else:
+            self.epsilon = epsilon
+
+    def uppdate_q_table(self, old_state, action, reward, new_state):
+        self.q_table[old_state][action] += self.alpha * (reward + self.gamma * np.max(self.q_table[new_state]) - self.q_table[old_state][action])
+
 
     def reduce(self, state):
         ratios = [(state[i] + abs(self.lower_bounds[i])) / (abs(self.upper_bounds[i]) - self.lower_bounds[i]) for i in range(self.state_length)]
@@ -31,15 +56,19 @@ class Simulation():
 
     def run(self):
         for i in range(self.num_episodes):
-            state = self.reduce(self.env.reset())
+            old_state = self.reduce(self.env.reset())
 
             while True:
                 self.env.render()
                 action = self.choose_action(state)
                 state, reward, done, info = self.env.step(action)
-                state = self.reduce(state)
-                sleep(0.1)
+                new_state = self.reduce(state)
+                self.update_q_table(self, old_state, action, reward, new_state)
+                old_state = new_state
+                sleep(0.01)
                 if done:
+                    self.calc_epsilon()
+                    self.calc_alpha()
                     break
 
         self.env.close()
