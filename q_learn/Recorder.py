9f88291335979ff13ea44b026f63d7637ac24468
Sean Pollock <seanpollock3344@gmail.com>
2018-03-21 22:29:52 -0400

    automates gif creation from environment monitor

--- q_learn/Recorder.py
+++ q_learn/Recorder.py
@@ -1,7 +1,9 @@
 import os
+import codecs
 import gym
 from gym import wrappers
-
+from moviepy.editor import *
+from time import sleep
 
 class Recorder():
     def __init__(self):
@@ -12,10 +14,27 @@ class Recorder():
             os.mkdir('./simulations')
 
     def wrap_env(self, env):
-        random_id = os.urandom(4)
-        return wrappers.Monitor(env, './simulations/{}'.format(random_id))
+        self.random_id = codecs.encode(os.urandom(4), 'hex').decode()
+        return wrappers.Monitor(env, './simulations/{}'.format(self.random_id))
 
+    def make_gif(self):
+        video_file = self.get_mp4_file()
+        print('video_file', video_file)
+        sleep(1)
+        self.check_file_size(video_file)
+        clip = (VideoFileClip('./simulations/{0}/{1}'.format(self.random_id, video_file)))
+        clip.write_gif('./simulations/{0}/sim.gif'.format(self.random_id))
 
+    def get_mp4_file(self):
+        files = os.listdir('./simulations/{0}/'.format(self.random_id))
+        for file_name in files:
+            if file_name[-3:] == 'mp4':
+                return file_name
+        raise Exception('no video file available')
+
+    def check_file_size(self, video_file):
+        stat_info = os.stat('./simulations/{0}/{1}'.format(self.random_id, video_file))
+        print('size', stat_info.st_size)
 
 if __name__ == '__main__':
     recorder = Recorder()
@@ -30,3 +49,5 @@ if __name__ == '__main__':
             break
 
     env.close
+    env._close_video_recorder()
+    recorder.make_gif()
