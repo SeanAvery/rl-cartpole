0802a7f83a3e4fb9fd8708608141830995b79ad3
Sean Pollock <seanpollock3344@gmail.com>
2018-03-23 16:08:09 -0400

    linear decay of reward function based on cart position

--- q_learn/Simulation.py
+++ q_learn/Simulation.py
@@ -3,17 +3,19 @@ import random
 from time import sleep
 import numpy as np
 from Graph import Graph
+from Recorder import Recorder
 
 class Simulation():
-    def __init__(self, graph):
+    def __init__(self, graph, recorder):
         # graph utility for results
         self.Graph = graph
 
+        self.Recorder = recorder
         # environment
-        self.env = gym.make('CartPole-v0')
+        self.env = self.Recorder.wrap_env(gym.make('CartPole-v0'))
 
         # state
-        self.buckets = (3, 2, 8, 8)
+        self.buckets = (1, 1, 8, 16)
         self.upper_bounds = [self.env.observation_space.high[0], 3.4, self.env.observation_space.high[2], 3.4]
         self.lower_bounds = [self.env.observation_space.low[0], -3.4, self.env.observation_space.low[2], -3.4]
         self.state_length = len(self.lower_bounds)
@@ -21,10 +23,10 @@ class Simulation():
         # model
         self.epsilon = 1
         self.min_epsilon = 0.05
-        self.epsilon_decay = 0.999
+        self.epsilon_decay = 0.99
         self.alpha = 1
         self.min_alpha = 0.05
-        self.alpha_decay = 0.999
+        self.alpha_decay = 0.99
         self.gamma = 0.995
         self.q_table = np.zeros(self.buckets + (self.env.action_space.n,))
 
@@ -52,10 +54,16 @@ class Simulation():
         reduced_state = [int((self.buckets[i] - 1) * ratios[i]) for i in range(self.state_length)]
         return tuple(reduced_state)
 
+    def calc_reward(self, raw_state):
+        return 10 - abs(raw_state[0] * 10)
+
     def run(self, num_episodes, isTraining):
         if not isTraining:
             self.Graph.init_results_graph()
+
+        # fixing strange race conditions with gym env
         sleep(0.1)
+
         for i in range(num_episodes):
             old_state = self.reduce(self.env.reset())
             ticks = 0
@@ -65,6 +73,8 @@ class Simulation():
                     self.env.render()
                 action = self.choose_action(old_state)
                 state, reward, done, info = self.env.step(action)
+                reward = self.calc_reward(state)
+                print('reward', reward)
                 new_state = self.reduce(state)
                 self.update_q_table(old_state, action, reward, new_state)
                 old_state = new_state
@@ -77,15 +87,18 @@ class Simulation():
 
         self.env.close()
 
-        # if not isTraining:
-        #     self.Graph.save_to_gif()
+        if not isTraining:
+            # self.Graph.save_to_gif()
+            self.Recorder.make_gif()
 
 if __name__ == '__main__':
     # init graph
     graph = Graph()
+    # init recorder
+    recorder = Recorder()
     # init simulation
     simulation = Simulation(graph)
     # run training
-    simulation.run(3000, True)
+    simulation.run(400, True)
     # run tests
-    simulation.run(100, False)
+    simulation.run(20, False)
